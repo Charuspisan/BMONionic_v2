10001a6b7d9c6cc2514daf88ee49825ee2dc3ae6
charuspisan <charuspisan@gmail.com>
2016-09-25 20:18:06 +0700

    start ionic

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('BMON', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,19 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('login', {
+      url:'/login',
+      templateUrl:'templates/login.html'
+    })
+    .state('landing', {
+      url:'/landing',
+      templateUrl:'templates/landing.html'
+    })
+
+  $urlRouterProvider.otherwise('/login')  
+
 })
